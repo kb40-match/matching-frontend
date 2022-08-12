3af9b34f4ec68e8d390a1873273645fb69537313
Jiwon <peace.cheejecake@gmail.com>
2022-08-12 16:29:49 +0900

    foundation: chat backend

--- chat-backend/src/index.js
+++ chat-backend/src/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import path from "path";
+import socketio from "socket.io";
+// import fs from "fs";
+import http from "http";
 
 const app = express();
 
@@ -8,8 +11,19 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.arguments(express.static(publicDirectoryPath));
 
+const server = http.createServer(app);
+const io = socketio.listen(server);
+
+app.get("/", (req, res) => {
+  
+});
+
+io.sockets.on("connection", (socket) => {
+  socket.on("message", (data) => {
+    io.sockets.emit("message", data);
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
-
-export { app };
--- chat-backend/src/socket.js
+++ chat-backend/src/socket.js
@@ -1,4 +1,42 @@
-import { app } from ".";
+import socketio from "socket.io";
+import http from "http";
 
-const server = http.createServer(app);
-const io = socketio(server);
+export const initSocket = (app) => {
+  const server = http.createServer(app);
+  const socket = socketio(server);
+
+  socket.on('join', ({username, room}, callback) => {
+    const { error, user } = addUser({ id: socket.id, username, room })
+
+    if (error) {
+        return callback(error) // acknoledgement. error를 client side로 보냄.
+    }
+    socket.join(user.room)
+
+    socket.emit('message', generateMessage({text: 'Welcome!', username: 'Admin'}))
+    socket.broadcast.to(user.room).emit('message', generateMessage({text: `${user.username} has joined!`, username: 'Admin'}))
+    io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+    })
+    
+    callback() // acknowledgement without error
+})
+
+  // socket.on("connection", socket => {
+  //   // either with send()
+  //   socket.send("Hello!");
+  
+  //   // or with emit() and custom event names
+  //   socket.emit("greetings", "Hey!", { "ms": "jane" }, Buffer.from([4, 3, 3, 1]));
+  
+  //   // handle the event sent with socket.send()
+  //   socket.on("message", (data) => {
+  //     console.log(data);
+  //   });
+  
+  //   socket.on("salutations", (elem1, elem2, elem3) => {
+  //     console.log(elem1, elem2, elem3);
+  //   });
+  // });
+};
