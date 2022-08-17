22b1c1039e1a13c90874a040d9e03d99f101688d
byunsangun <xdcfvgg@naver.com>
2022-08-17 12:36:45 +0900

    Implement validation check for user ID

--- frontend/src/components/UserSelect/UserSelect.vue
+++ frontend/src/components/UserSelect/UserSelect.vue
@@ -6,22 +6,50 @@
             <input id="text-field-userid" v-model="userId" placeholder="ID를 입력하세요." >
             <v-btn text rounded large id="loginBtn" @click="onSubmitForm" style="background-color: #845ef7;">로그인</v-btn>
         </div>
+
+        <v-snackbar v-model="alert" top flat color="#f1f3f5" rounded="pill" :timeout="1500" style="margin-top:70px;font-size:20px;">
+        <span id="alertMessage">
+            {{this.message}}
+        </span>
+        </v-snackbar>
+
     </div>
 </template>
 
 <script>
+import { useAppStore } from '../../store/userState'
+import { loadUser, loadMydata } from "../../worker/user";
+
 export default {
+    setup() {
+        const store = useAppStore()
+        return { store }
+  },
     data () {
         return {
-            userId: ""
+            userId: "",
+            alert: false,
+            message: "user1~200 사이의 형태로 입력해주세요."
         }
     },
     methods:{
         onSubmitForm() {
-            console.log(this.userId)
-
-            // TODO: go to next page
-            
+            if(this.check()) {
+                this.$userId = this.userId
+                loadUser(this.$userId)
+                loadMydata(this.$userId)
+                this.$router.push("/myDataAgree").catch(() => {})
+            } 
+            else {
+                this.alert = true               
+            }  
+        },
+        check() {
+            if(this.userId.substring(0, 4) === "user" && parseInt(this.userId.substring(4, this.userId.length)) <= 200) {
+                console.log(parseInt(this.userId.substring(4, this.userId.length)))
+                return true
+            }
+            else { return false}
         }
     }
 }
@@ -35,19 +63,32 @@ export default {
     text-align: center;
     font-weight: normal;
     font-size: 24px;
-    margin-top: 30px;
+    margin-top: 150px;
     margin-bottom:30px;
 }
 
 #box-main {
     display: flex;
     flex-direction: column;
-    margin-top: 50px;
+    margin-top: 100px;
+}
+
+#text-field-userid {
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #dee2e6;
+    border-radius: 5px;
+    padding-left: 15px;
+    padding-right: 15px;
+    
 }
 
 #loginBtn {
     margin-top: 50px;
     color: white;
+    font-size: 20px;
 }
 
 
